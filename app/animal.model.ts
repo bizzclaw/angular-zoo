1ba395b1213fb8454d7bd993aaf01aefb8cec324
Joseph Tomlinson <bizzclaw1@gmail.com>
2017-12-08 13:42:24 -0800

    add Type Enforcement for animal data types.

--- app/animal.model.ts
+++ app/animal.model.ts
@@ -5,6 +5,29 @@ export class Animal {
   // DATA TYPES //
   //These Data types will auto fill themselves into edit/creation forms. Instad of adding that info to both edit and creation forms, they can be pulled directly from this list.
   //dataString values are objects with different properties.
+
+  public static typeEnforcements = {
+    ["number"]: function(input) {
+      return parseInt(input)
+    },
+    ["date"]: function(input) {
+      return new Date(input);
+    }
+  }
+
+  static EnforceTypes(data) {
+    let keys = Object.keys(data)
+    keys.forEach(function(k) {
+      let v = data[k]
+      let dataInfo = Animal.dataTypes[k]
+
+      let typeEnforcement = Animal.typeEnforcements[dataInfo.type]
+      data[k] =  typeEnforcement? typeEnforcement(data[k]) : data[k]
+    });
+    console.log(data);
+    return data
+  }
+
   public static dataTypes: Object = {
     ["name"]: {lang: "Name", type: "string"},
     ["species"]: {lang: "Species", type: "string"},
@@ -12,18 +35,20 @@ export class Animal {
     ["dislikes"]: {lang: "Dislikes", type: "string"},
     ["location"]: {lang: "Location", type: "string"},
     ["diet"]: {lang: "Diet", type: "string"},
+    ["dob"]: {lang: "Date of Birth", type: "date"},
     ["caretakers"]: {lang: "Needed Caretakers", type: "number", min: 1, max: 25},
     ["sex"]: {lang: "Sex", type: "number", min: 0, max: 1, selections: ["Male", "Female"]},
   }
-
-
 
   static getAll() {
     return Animal.animals;
   }
 
   public id: number;
-  constructor(public data: object) {};
+  public data: object;
+  constructor(data: object) {
+    this.data = Animal.EnforceTypes(data);
+  };
 
   save() {
     this.id = Animal.animals.length;
